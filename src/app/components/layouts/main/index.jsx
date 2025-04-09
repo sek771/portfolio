a09852f6e8f88456e2b747dc45bbde7fe0c723ec
sek771 <sackos77100@gmail.com>
2025-04-10 00:25:23 +0200

    update

--- src/app/components/layouts/main/index.jsx
+++ src/app/components/layouts/main/index.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 
-const Main = ({ className, children }) => {
-  return <main className={`${className}`}>{children}</main>;
+const Main = ({ className = "", children }) => {
+  return <main className={className}>{children}</main>;
 };
 
 Main.propTypes = {
-  className: PropTypes.string,
+  className: PropTypes.string, // Vérifie que className est une chaîne
+  children: PropTypes.node, // Vérifie que children est un élément React valide
 };
 
 export default Main;
