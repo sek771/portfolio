aa1fdf7a0107aeff4c6eb58ae6a2c4a77f1ccbf8
sek771 <sackos77100@gmail.com>
2025-04-10 07:59:16 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,17 @@
+"use client";
+
 import Header from "@/app/components/layouts/header";
 import Home from "@/app/home";
 import Footer from "@/app/components/layouts/footer";
-import Scroll from "./components/tools/scroll to top";
 
 const HomePage = () => {
   return (
     <>
-      <Scroll />
       <Header />
       <Home />
       <Footer />
     </>
   );
 };
+
 export default HomePage;
